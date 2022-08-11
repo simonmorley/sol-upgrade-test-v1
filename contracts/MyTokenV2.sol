fc36249ed32a7305ae61c9bdbb4a48fb0c93f234
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-08-11 17:35:45 +0100

    simon being a tool

--- contracts/MyTokenV2.sol
+++ contracts/MyTokenV2.sol
@@ -3,32 +3,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract MyTokenV2 is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
-    uint public num;
+contract MyTokenV2 is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, AccessControlUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
     bool blacklist;
 
-    function initialize() public initializer {
-        __ERC20_init('MyToken', 'MTK');
-
+    function initialize(string memory name, string memory symbol) initializer public {
+        __ERC20_init(name, symbol);
         __Ownable_init();
+        __UUPSUpgradeable_init();
 
-        _mint(msg.sender, 1000 * 10**decimals());
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    // only ADMIN!!
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
-    function add(uint _num) public {
-      num += _num;
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "invalid-minter");
+        _mint(to, amount);
+    }
+
+    function addMinter(address _address) public {
+        grantRole(MINTER_ROLE, _address);
+    }
+
+    function removeMinter(address _address) public {
+        revokeRole(MINTER_ROLE, _address);
+    }
+
+    function burn(address from, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "invalid-burner");
+        _burn(from, amount);
+    }
+
+    function addBurner(address _address) public {
+        grantRole(BURNER_ROLE, _address);
+    }
+
+    function removeBurner(address _address) public {
+        revokeRole(BURNER_ROLE, _address);
     }
 
     function transfer(address to, uint256 value) public override returns (bool) {
         require(blacklist, 'oh-dear');
-        super.transfer(to, value);
+        return super.transfer(to, value);
     }
-
 }
--- test/test.ts
+++ test/test.ts
@@ -4,15 +4,14 @@ const { ethers, upgrades } = require('hardhat');
 describe('MyToken', function () {
   it('deploys', async function () {
     const MyTokenV1 = await ethers.getContractFactory('MyTokenV1');
-    const one = await upgrades.deployProxy(MyTokenV1, { kind: 'uups' });
+    const kind = 'uups';
+    const one = await upgrades.deployProxy(MyTokenV1, ['SEURO','SEURO'], { kind });
     await one.deployed();
 
     const MyTokenV2 = await ethers.getContractFactory('MyTokenV2')
     const two = await upgrades.upgradeProxy(one.address, MyTokenV2)// as MyTokenV2
     await two.deployed();
     
-    expect (await two.add(1)).to.not.be.reverted;
-
     expect(two.address).to.eq(one.address);
   });
 });
